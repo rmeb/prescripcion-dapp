994e3ec5245b5dfc9e7de18e451018e0ac28a31b
coloro <colorolabs@gmail.com>
2018-07-06 19:45:55 -0400

    qr

--- src/screens/PrescriptionSuccess.js
+++ src/screens/PrescriptionSuccess.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react'
 
+const QRious= window.QRious
+
 export default class PrescriptionSuccess extends Component {
+  componentDidMount() {
+    new QRious({element: document.getElementById('qr-code'), value: this.props.match.params.code, size: 200})
+  }
+
   render() {
     return (
-      <div className="text-center">
-        <p>Receta emitida, tu codigo es:</p>
-        <h5 className="display-4">{this.props.match.params.code}</h5>
+      <div className="card">
+        <div className="card-body text-center">
+          <p>Receta emitida, tu codigo es:</p>
+          <h5 className="display-4">{this.props.match.params.code}</h5>
+          <canvas id="qr-code"></canvas>
+        </div>
       </div>
     )
   }
